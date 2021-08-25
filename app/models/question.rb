a4655eeb1d2a81c6953a2117f73f2c0e2ed6c299
Barsbarloo <lootanke@gmail.com>
2021-08-25 16:19:25 +0600

    Update validation, syntax error

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,4 @@
 class Question < ApplicationRecord
-    validates :title, presence: true, lenght {minimum: 2}
-    validates :body, presence: true, lenght {minimum: 2}
+    validates :title, presence: true, length: {minimum: 2}
+    validates :body, presence: true, length: {minimum: 2}
 end
