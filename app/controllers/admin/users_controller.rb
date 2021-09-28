3de75b0f36a533b75c7590b1d0c03799b3b225aa
Barsbarloo <lootanke@gmail.com>
2021-09-28 16:12:46 +0600

    Add  method to read format .html

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,6 +2,17 @@ class Admin::UsersController < ApplicationController
   before_action :require_authentication
 
   def index
-    @pagy, @users = pagy User.order(created_at: :desc)
+    respond_to do |format|
+      format.html do
+        @pagy, @users = pagy User.order(created_at: :desc)
+      end
+
+      format.zip {respond_with_zipped_users}
+    end
+  end
+  private
+  
+  def respond_with_zipped_users
+
   end
 end
