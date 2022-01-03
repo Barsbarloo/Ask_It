f62716a11ee2bfa04031714668c45f1e18ef0464
Barsbarloo <lootanke@gmail.com>
2022-01-03 23:11:32 +0600

    Add gravatar migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_08_151252) do
+ActiveRecord::Schema.define(version: 2022_01_03_171018) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_12_08_151252) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "remember_token_digest"
+    t.string "gravatar_hash"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
